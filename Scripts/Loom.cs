655274fc6faafddffe7e4bbe4b29cc7f2ca0e6db
Michael Cann <mike.cann@gmail.com>
2014-11-19 16:33:43 +0800

    refactored the loom class to make it easyier to read, loom now checks to see if a loom exists in the world already, loom now can queue with a conditional function

--- Scripts/Loom.cs
+++ Scripts/Loom.cs
@@ -5,16 +5,19 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using UnityEngine;
+using UnityEngine.Events;
 
 namespace UnityParseHelpers
 {
     public interface ILoom
     {
+        Task Delay(float seconds);
         Task Delay(TimeSpan time);
         void Clear();
         Thread RunAsync(Action a);
         void QueueOnMainThread(Action action);
         void QueueOnMainThread(Action action, float time);
+        int QueuedItemCount { get; }
     }
 
     public class Loom : MonoBehaviour, ILoom
@@ -28,27 +31,33 @@ namespace UnityParseHelpers
 
         private List<Action> _actions = new List<Action>();
         private List<DelayedQueueItem> _delayed = new List<DelayedQueueItem>();
+        private List<ConditionalQueueItem> _conditional = new List<ConditionalQueueItem>();
 
         private List<DelayedQueueItem> _currentDelayed = new List<DelayedQueueItem>();
         private List<Action> _currentActions = new List<Action>();
+        private List<ConditionalQueueItem> _currentConditional = new List<ConditionalQueueItem>();
 
         private static Loom _instance;
         public static Loom Instance
         {
             get 
             {
-                if(_instance==null)
-                    _instance = new GameObject("Loom").AddComponent<Loom>();
+                if (_instance == null) _instance = GameObject.FindObjectOfType<Loom>();
+                if(_instance==null) _instance = new GameObject("Loom").AddComponent<Loom>();
                 return _instance;
             }
         }
 
-        private int loomId = 0;
-
-        public static void Init()
+        public int QueuedItemCount
         {
-            if (_instance != null) return;
-            _instance = new GameObject("Loom").AddComponent<Loom>();
+            get
+            {
+                var total = 0;
+                lock (_actions) { total += _actions.Count; }
+                lock (_delayed) { total += _delayed.Count; }
+                lock (_conditional) { total += _conditional.Count; }
+                return total;
+            }
         }
 
         public struct DelayedQueueItem
@@ -57,45 +66,50 @@ namespace UnityParseHelpers
             public Action action;
         }
 
+        public struct ConditionalQueueItem
+        {
+            public TaskCompletionSource<bool> tcs;
+            public Func<bool> condition;
+        }
+
         protected virtual void Start()
         {
             m_HasLoaded = true;
             DontDestroyOnLoad(gameObject);
         }
 
+        public Task Delay(float seconds)
+        {
+            return Delay(TimeSpan.FromSeconds(seconds));
+        }
+
         public Task Delay(TimeSpan time)
         {
-            var lid = loomId;
             var tcs = new TaskCompletionSource<bool>();
-            Task.Delay(time).Then(t =>
+            QueueOnMainThread(() => tcs.SetResult(true), (float)time.TotalSeconds);            
+            return tcs.Task;
+        }
+
+        public Task Until(Func<bool> condition)
+        {
+            var tcs = new TaskCompletionSource<bool>();
+            lock (_conditional)
             {
-                // If the id of the loom has changed since the delay then dont go any further
-                if (loomId != lid)
-                {
-                    Debug.Log("Loom ID not equal, probably a scene change so going not executing delayed task");
-                    return;
-                }
-                QueueOnMainThread(() => tcs.SetResult(true));
-            });
+                _conditional.Add(new ConditionalQueueItem {
+                    condition = condition,
+                    tcs = tcs
+                });
+            }
             return tcs.Task;
         }
 
         public void Clear()
         {
-            // Increment the loom ID so that delayed tasks dont get run
-            loomId++;
-
-            lock (_delayed)
-            {
-                loomId++;
-                Debug.Log(_delayed.Count + " delayed actions cleared from loomer.");
-                _delayed.Clear();
-            }
-            lock (_actions)
-            {
-                Debug.Log(_actions.Count + " actions cleared from loomer.");
-                _actions.Clear();
-            }
+            var total = QueuedItemCount;
+            lock (_delayed) { _delayed.Clear(); }
+            lock (_actions) { _actions.Clear(); }
+            lock (_conditional) { _conditional.Clear(); }
+            Debug.Log(total + " actions cleared from loomer.");
         }
 
         public void QueueOnMainThread(Action action)
@@ -123,10 +137,7 @@ namespace UnityParseHelpers
 
         public Thread RunAsync(Action a)
         {
-            while (numThreads >= maxThreads)
-            {
-                Thread.Sleep(1);
-            }
+            while (numThreads >= maxThreads) Thread.Sleep(1);
             Interlocked.Increment(ref numThreads);
             ThreadPool.QueueUserWorkItem(RunAction, a);
             return null;
@@ -152,16 +163,13 @@ namespace UnityParseHelpers
             if (m_HasLoaded == false)
                 Start();
 
-            lock (_actions)
-            {
-                _currentActions.Clear();
-                _currentActions.AddRange(_actions);
-                _actions.Clear();
-            }
-            foreach (var a in _currentActions)
-            {
-                a();
-            }
+            HandleActions();
+            HandleDelayed();
+            HandleConditional();
+        }
+
+        private void HandleDelayed()
+        {
             lock (_delayed)
             {
                 _currentDelayed.Clear();
@@ -172,6 +180,51 @@ namespace UnityParseHelpers
             foreach (var delayed in _currentDelayed)
             {
                 delayed.action();
+            }
+        }
+
+        private void HandleActions()
+        {
+            lock (_actions)
+            {
+                _currentActions.Clear();
+                _currentActions.AddRange(_actions);
+                _actions.Clear();
+            }
+            foreach (var a in _currentActions)
+            {
+                a();
+            }
+        }
+
+        private void HandleConditional()
+        {
+            // Lock the conditional and perform minimum amount of work
+            // so that we dont deadlock anything
+            lock (_conditional)
+            {
+                _currentConditional.Clear();
+                _currentConditional.AddRange(_conditional);
+            }
+
+            // Work out which conditionals have met their condition
+            var toRemove = new List<ConditionalQueueItem>();
+            foreach (var conditional in _currentConditional)
+            {
+                // If the condition is met then return the task
+                if (conditional.condition())
+                {
+                    conditional.tcs.SetResult(true);
+                    toRemove.Add(conditional);
+                }                    
+            }                
+
+            // Once the condtition has been met it can be removed so that next 
+            // update it isnt checked again
+            lock (_conditional)
+            {
+                foreach (var c in toRemove)
+                    _conditional.Remove(c);
             }
         }
 
